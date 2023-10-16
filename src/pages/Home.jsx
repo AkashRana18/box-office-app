1076be661eb777db1f7cc2193642f0000d4ada3d
AkashRana18 <akashmanu2003@gmail.com>
2023-10-17 01:31:11 +0530

    Installed react-router-dom

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+const Home = () => {
+
+    return (
+        <div>
+        <Link to = "/starred "> Click Here </Link>
+        <br/>
+        <Link to = "/about "> Click Here </Link>
+        </div>
+    )
+}
+export default Home;
---
+++ src/pages/Starred.jsx
@@ -0,0 +1,8 @@
+const Starred = () => {
+    return (
+        <div>
+        <h1>Starred Page</h1>
+        </div>
+    )
+}
+export default Starred;
